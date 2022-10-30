a200db83e682d8ecc47707f5b2bc1f2988d4b3fa
panchasan <joanna.walowska@wp.pl>
2022-10-30 17:18:25 +0100

    initialization

--- proj_container/setup.sh
+++ proj_container/setup.sh
@@ -2,6 +2,10 @@ sudo apt-get update
 
 sudo apt-get -y install terminator
 sudo apt-get -y install libasio-dev
+pip install numpy --upgrade
+pip install librosa
+
+
 
 rosdep update
 rosdep install --from-paths ${HOME}/drone_ws/src --ignore-src -yr
@@ -11,3 +15,5 @@ echo "source ${HOME}/drone_ws/install/setup.bash" >> ${HOME}/.bashrc
 sudo usermod -G dialout -a ${USER}
 sudo usermod -G root -a ${USER}
 source ${HOME}/.bashrc
+
+
