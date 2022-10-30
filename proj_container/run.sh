193df9457bdd30efd504f38f6ce37c4631c5c9e8
panchasan <joanna.walowska@wp.pl>
2022-10-30 16:42:07 +0100

    initialization

--- proj_container/run.sh
+++ proj_container/run.sh
@@ -1,6 +1,5 @@
 rocker --network host --privileged --nvidia --x11 --user --name drone_ws \
   	--env="USER" \
 	--volume /dev/shm \
-	--volume $HOME/Studia/ARL-Retinger/drone_ws:$HOME/drone_ws \
-	--volume /opt/pycharm-community-2022.1.1:/opt/pycharm \
+	--volume $HOME/ARL/ARL_project:$HOME/drone_ws \
 	-- tello_ros:foxy
