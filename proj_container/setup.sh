e64b7bc35e20a261d2ffb4fdd198b6418b99a34d
krzysztof-nowicki <maromixpl@gmail.com>
2023-01-08 16:59:21 +0100

    src and setup.bash changes

--- proj_container/setup.sh
+++ proj_container/setup.sh
@@ -10,3 +10,8 @@ echo "source ${HOME}/drone_ws/install/setup.bash" >> ${HOME}/.bashrc
 sudo usermod -G dialout -a ${USER}
 sudo usermod -G root -a ${USER}
 source ${HOME}/.bashrc
+pip install asyncio
+pip install google
+pip install --upgrade google-api-python-client
+pip install protobuf==3.20.3
+
