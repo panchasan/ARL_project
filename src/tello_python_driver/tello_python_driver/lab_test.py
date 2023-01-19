0c1dc76e032436043a6b259f4375fed0518104ea
tomaszkapron <tomasz.a.kapron@gmail.com>
2023-01-19 11:35:46 +0100

    feat: restrictions for lab dimentions, twiks on flying

--- src/tello_python_driver/tello_python_driver/lab_test.py
+++ src/tello_python_driver/tello_python_driver/lab_test.py
@@ -31,10 +31,10 @@ class MinimalPublisher(Node):
         self.dest_x = 0.0
         self.dest_y = 0.0
         self.dest_z = 1.0
-        self.Data = np.genfromtxt("traj.txt", dtype=float,
+        self.Data = np.genfromtxt("traj_adele.txt", dtype=float,
                         encoding=None, delimiter=",")
         self.num_lines = 0
-        with open(r"traj.txt", 'r') as fp:
+        with open(r"traj_adele.txt", 'r') as fp:
             self.num_lines = len(fp.readlines())
         timer_period = self.Data[self.num_lines-1][0]/self.num_lines  # seconds
         print(timer_period)
@@ -42,37 +42,36 @@ class MinimalPublisher(Node):
         self.i = 0
         print("Let's go 😎️🤯️😵️")
         pygame.mixer.init()
-        pygame.mixer.music.load("hp.mp3")
+        pygame.mixer.music.load("Adele.mp3")
         pygame.mixer.music.play()
-
 
     def listener_callback(self, msg):
         
         self.x = msg.position.x
         self.y = msg.position.y
         self.z = msg.position.z
-        if abs(self.dest_x - self.x) > 0.05 or abs(self.dest_y - self.y) > 0.05 or abs(self.dest_z - self.z) > 0.05:
+        if abs(self.dest_x - self.x) > 0.1 or abs(self.dest_y - self.y) > 0.1 or abs(self.dest_z - self.z) > 0.1:
             self.parameters.linear.x = (self.dest_x - self.x)
             self.parameters.linear.y = (self.dest_y - self.y) 
             self.parameters.linear.z = (self.dest_z - self.z) 
-            if self.parameters.linear.x >0.2:
-                self.parameters.linear.x = 0.2
-            if self.parameters.linear.y >0.2:
-                self.parameters.linear.y = 0.2
-            if self.parameters.linear.z >0.2:
-                self.parameters.linear.z = 0.2
-            if self.parameters.linear.x < -0.2:
-                self.parameters.linear.x = -0.2
-            if self.parameters.linear.y < -0.2:
-                self.parameters.linear.y = -0.2
-            if self.parameters.linear.z < -0.2:
-                self.parameters.linear.z = -0.2
+            if self.parameters.linear.x > 0.3:
+                self.parameters.linear.x =  0.3
+            if self.parameters.linear.y > 0.3:
+                self.parameters.linear.y =  0.3
+            if self.parameters.linear.z >0.4:
+                self.parameters.linear.z = 0.4
+            if self.parameters.linear.x < - 0.3:
+                self.parameters.linear.x = - 0.3
+            if self.parameters.linear.y < - 0.3:
+                self.parameters.linear.y = - 0.3
+            if self.parameters.linear.z < -0.4:
+                self.parameters.linear.z = -0.4
                 
-            if self.parameters.linear.x <0.05 and self.parameters.linear.x >-0.05 :
+            if self.parameters.linear.x <0.1 and self.parameters.linear.x >-0.1 :
                 self.parameters.linear.x = 0.0
-            if self.parameters.linear.y <0.05 and self.parameters.linear.y >-0.05 :
+            if self.parameters.linear.y <0.1 and self.parameters.linear.y >-0.1 :
                 self.parameters.linear.y = 0.0
-            if self.parameters.linear.z <0.05 and self.parameters.linear.z >-0.05 :
+            if self.parameters.linear.z <0.1 and self.parameters.linear.z >-0.1 :
                 self.parameters.linear.z = 0.0
 
             self.publisher_parameters.publish(self.parameters)
@@ -83,6 +82,7 @@ class MinimalPublisher(Node):
             self.publisher_parameters.publish(self.parameters)
 
             time.sleep(0.1)
+            
     def clear(self):
         self.parameters.linear.x = 0.0
         self.parameters.linear.y = 0.0
@@ -90,6 +90,7 @@ class MinimalPublisher(Node):
         self.parameters.angular.x = 0.0
         self.parameters.angular.y = 0.0
         self.parameters.angular.z = 0.0
+        
     def timer_callback(self):
         if self.i < self.num_lines - 1:
             print('********************')
@@ -99,11 +100,24 @@ class MinimalPublisher(Node):
             self.dest_y=self.Data[self.i][2]
             self.dest_z=self.Data[self.i][3]
             self.i=self.i+1
+            
+            if self.dest_x < -1:
+                self.dest_x = -1
+            if self.dest_x > 1:
+                self.dest_x = 1
+            
+            if self.dest_y < -1:
+                self.dest_y = -1
+            if self.dest_y > 1:
+                self.dest_y = 1
+            
+            if self.dest_z < 0.3:
+                self.dest_z = 0.3
+            if self.dest_z > 1.7:
+                self.dest_z = 1.7    
+            
         else:
             print("********NO NEW DATA********")
-
-
-
 
 
 def main(args=None):
